1482d21f7c1c10aa945c727a76844c9481e9f909
alinap1234 <alina.pollner@gmail.com>
2018-06-04 21:30:14 +0000

    A: alina is the best

--- quiz.c
+++ quiz.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main()
+{
+
+char category[400];
+char input[500];
+
+printf("Hi! Welcome to Quizzy, all about the dopest facts in the world!\n");
+printf("We're about to get going, what category would you like?\n Here are your options:\n Gross\n Kardashians\n Countries\n Animals\n Natalee and Alina\n");
+
+fgets(input, 256, stdin);
+sscanf(input, "%s", &category);
+
+if (strcmp ("gross", category) == 0) 
+	{
+	printf("OK, your chose category is %s, let's load up your first question.\n", category);
+	//randquest
+
+	}
+
+
+
+
+
+}
