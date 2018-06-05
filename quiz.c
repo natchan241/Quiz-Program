542e368dbd8e6c4e81952172de44a02aef974824
alinap1234 <alina.pollner@gmail.com>
2018-06-05 21:55:16 +0000

    A:

--- quiz.c
+++ quiz.c
@@ -8,6 +8,9 @@ int main()
 char category[400];
 char input[500];
 char answerq1[40];
+FILE *file;
+
+
 
 printf("Hi! Welcome to Quizzy, all about the dopest facts in the world!\n");
 printf("We're about to get going, what category would you like?\n Here are your options:\n Gross\n Kardashians\n Countries\n Animals\n Natalee and Alina\n");
@@ -28,9 +31,14 @@ if (strcmp ("Gross", category) == 0)
         printf("Why are jelly beans so shiny? (what are they covered with?)\n");
         fgets(input, 256, stdin);
         sscanf(input, "%s", &answerq1);
+	file = fopen("answers", "wt");
+	fprintf(file, "Your nswer to first question is: %s\n The Correct Answer is: shellac", answerq1);
+	
+
 	if (strcmp ("shellac", answerq1) == 0)
-	printf("Correct! It's actually made from insect poop!\n");
-	else printf("You're wrong, sorry bro\n")
+	{printf("Correct! It's actually made from insect poop!\n");
+	fprintf(file, "Correct!\n");}
+	else printf("You're wrong, sorry bro\n");
 	}
 
 
