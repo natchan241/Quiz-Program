9220cdabc719bb9ffac0c00bb5e01860f9884328
natchan241 <channatalee@gmail.com>
2018-06-08 21:41:55 +0000

    N: Version 4

--- makequiz/quizfuncs.c
+++ makequiz/quizfuncs.c
@@ -3,16 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-void printAnswersByPtr(struct Answer* Answer)
-{
-  printf("RESULTS:\n");
-  printf("  Category Chosen: %s\n", Answer->category);
-  printf("  Number of Questions: %d\n", Answer->numbquest);
-  printf("  Number of Questions Correct: %d\n", Answer->correct);
-  printf("  Number of Questions Incorrect: %d\n", Answer-> incorrect);
-  printf("To find out details on what you got on your quiz, visit: http://rpi09/nat241/quizappres.html");
-}
-
 void verifystring(char string[])
 {
   while (1)
@@ -52,7 +42,7 @@ void contingame(char choice[])
     
     if (strcmp("No", choice) == 0 || strcmp("no", choice) == 0)
     {
-      printf("Ok! Good job, here are the results!\n");
+      printf("Ok! Good job, nano/emacs answers to see your results! Or visit http://rpi09/nat241/quizappres.html\n");
       exit(0);
     }
     else if (strcmp("Yes", choice) == 0 || strcmp("yes", choice) == 0)
--- makequiz/quizfuncs.h
+++ makequiz/quizfuncs.h
@@ -1,12 +1,3 @@
-struct Answer {
-  char category[256];
-  int numbquest;
-  int correct;
-  int incorrect;
-};
-
-void printAnswersByPtr(struct Answer* Answer);
-
 void verifystring(char name[]);
 
 int verifyint(int a);
